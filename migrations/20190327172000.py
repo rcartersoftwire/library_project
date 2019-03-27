738e9ce1eff2cffd64ce3d5e5c8b1bf403752c5d
Zheng Hung <Zheng.Hung@softwire.com>
2019-03-27 17:23:27 +0000

    Fixed Migration changes

---
+++ migrations/20190327172000.py
@@ -0,0 +1,28 @@
+"""
+Caribou migration file
+"""
+
+
+def upgrade(connection):
+    # Adding Book Request Table
+    sql = """CREATE TABLE IF NOT EXISTS book_request(
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                title VARCHAR(255) NOT NULL,
+                author_first_name VARCHAR(255) NOT NULL,
+                author_last_name VARCHAR(255) NOT NULL
+                );"""
+
+    connection.execute(sql)
+
+
+    # Adding photo path
+    sql = """ALTER TABLE user ADD COLUMN
+             prof_pic VARCHAR(255) DEFAULT '/static/images/unknown-user.jpg';"""
+    connection.execute(sql)
+
+
+    connection.commit()
+
+
+def downgrade(connection):
+    pass
