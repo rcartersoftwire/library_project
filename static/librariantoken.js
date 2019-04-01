c4aa3790643341cc179712dfbfc5e2b45917c60a
Zheng Hung <Zheng.Hung@softwire.com>
2019-04-01 17:52:19 +0100

    Join Library: librarian token field type changed to password

--- static/librariantoken.js
+++ static/librariantoken.js
@@ -2,7 +2,7 @@ function askForToken() {
     var acc_type = $("#acc_type").val();
 
     if (acc_type == 1){
-        $("#divLibrarianToken").html("<div><label for=\"token\">Librarian Token</label><input type=\"token\" id=\"token\" name=\"token\" required=required></div>");
+        $("#divLibrarianToken").html("<div><label for=\"token\">Librarian Token</label><input type=\"password\" id=\"token\" name=\"token\" required=required></div>");
     }else{
         $("#divLibrarianToken").html("");
     }
